ea4149fc1d90c17b960ab96bcc565a3fbf48ef6c
RusssLee <ruslee.play@gmail.com>
2025-05-28 15:53:19 +0300

    voice_widgets_anim_added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,6 +10,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _activeIndex = -1;
+
+  static const double _normalSize = 70;
+  static const double _activeSize = 100;
+  static const Duration _duration = Duration(milliseconds: 250);
+
+  double get _scaleNormal => 1.0;
+  double get _scaleActive => _activeSize / _normalSize;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,22 +40,86 @@ class _HomeState extends State<Home> {
                   Positioned(
                     top: 680,
                     left: 30,
-                    child: VoiceRound(voiceTitle: "Flow"),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _activeIndex = 0;
+                        });
+                      },
+                      child: AnimatedScale(
+                        scale: _activeIndex == 0 ? _scaleActive : _scaleNormal,
+                        duration: _duration,
+                        curve: Curves.easeOut,
+                        child: SizedBox(
+                          width: _normalSize,
+                          height: _normalSize,
+                          child: VoiceRound(voiceTitle: "Flow"),
+                        ),
+                      ),
+                    ),
                   ),
                   Positioned(
                     top: 710,
                     left: 120,
-                    child: VoiceRound(voiceTitle: "Poem"),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _activeIndex = 1;
+                        });
+                      },
+                      child: AnimatedScale(
+                        scale: _activeIndex == 1 ? _scaleActive : _scaleNormal,
+                        duration: _duration,
+                        curve: Curves.easeOut,
+                        child: SizedBox(
+                          width: _normalSize,
+                          height: _normalSize,
+                          child: VoiceRound(voiceTitle: "Poem"),
+                        ),
+                      ),
+                    ),
                   ),
                   Positioned(
                     top: 710,
                     right: 120,
-                    child: VoiceRound(voiceTitle: "Silence"),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _activeIndex = 2;
+                        });
+                      },
+                      child: AnimatedScale(
+                        scale: _activeIndex == 2 ? _scaleActive : _scaleNormal,
+                        duration: _duration,
+                        curve: Curves.easeOut,
+                        child: SizedBox(
+                          width: _normalSize,
+                          height: _normalSize,
+                          child: VoiceRound(voiceTitle: "Silence"),
+                        ),
+                      ),
+                    ),
                   ),
                   Positioned(
                     top: 680,
                     right: 30,
-                    child: VoiceRound(voiceTitle: "Logic"),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _activeIndex = 3;
+                        });
+                      },
+                      child: AnimatedScale(
+                        scale: _activeIndex == 3 ? _scaleActive : _scaleNormal,
+                        duration: _duration,
+                        curve: Curves.easeOut,
+                        child: SizedBox(
+                          width: _normalSize,
+                          height: _normalSize,
+                          child: VoiceRound(voiceTitle: "Logic"),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
